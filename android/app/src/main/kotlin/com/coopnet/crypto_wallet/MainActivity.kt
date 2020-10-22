e9044fc44f1342fc79343616ef04a113dc598a72
Chrisogonas O. Mc'Odhiambo <chrisogonas@yahoo.com>
2020-10-21 23:55:21 -0400

    All updates the first time

---
+++ android/app/src/main/kotlin/com/coopnet/crypto_wallet/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.coopnet.crypto_wallet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/net/flutterfire.dart
@@ -0,0 +1,63 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+Future<bool> signIn(String email, String password) async {
+  try {
+    UserCredential userCredential = await FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: email, password: password);
+    return true;
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'user-not-found') {
+      print('No user found for that email.');
+    } else if (e.code == 'wrong-password') {
+      print('Wrong password provided for that user.');
+    }
+    return false;
+  } catch (e) {
+    print(e);
+    return false;
+  }
+}
+
+Future<bool> register(String email, String password) async {
+  try {
+    await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(email: email, password: password);
+    return true;
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'weak-password') {
+      print('The password provided is too weak.');
+    } else if (e.code == 'email-already-in-use') {
+      print('The account already exists for that email.');
+    }
+    return false;
+  } catch (e) {
+    print(e);
+    return false;
+  }
+}
+
+Future<bool> addCoin(String id, String amount) async {
+  try {
+    String uid = FirebaseAuth.instance.currentUser.uid;
+    var value = double.parse(amount);
+    DocumentReference documentReference = FirebaseFirestore.instance
+        .collection('Users')
+        .doc(uid)
+        .collection('Coins')
+        .doc(id);
+    FirebaseFirestore.instance.runTransaction((transaction) async {
+      DocumentSnapshot documentSnapshot =
+          await transaction.get(documentReference);
+          if(!documentSnapshot.exists) {
+            documentReference.set({'Amount': value});
+            return true;
+          }
+          double newAmount = documentSnapshot.data()['Amount'] + value;
+          transaction.update(documentReference, {'Amount': newAmount});
+          return true;
+    });
+  } catch (e) {
+    return false;
+  }
+}
